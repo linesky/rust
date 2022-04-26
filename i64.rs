0a65eedfaa00ed7625f9784c1e2991ebc72ac8bc
sky line <32512284+linesky@users.noreply.github.com>
2022-04-26 11:46:39 +0100

    --crate-type=staticlib

--- i64.rs
+++ i64.rs
@@ -1,4 +1,15 @@
-fn main(){
-	let a:i64=1234567890123456789;
-	println!("{}",a);
+fn toString(a:i64)->String{
+		return a.to_string();
+}
+fn add(a:i64,b:i64)->i64{
+	return a+b;
+}
+fn sub(a:i64,b:i64)->i64{
+	return a-b;
+}
+fn div(a:i64,b:i64)->i64{
+	return a/b;
+}
+fn mul(a:i64,b:i64)->i64{
+	return a*b;
 }
