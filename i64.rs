2c9bb3aef529b397e8b58775e71c467371c946db
sky line <32512284+linesky@users.noreply.github.com>
2024-04-09 12:13:49 +0100

    Add files via upload

--- i64.rs
+++ i64.rs
@@ -13,3 +13,9 @@ fn div(a:i64,b:i64)->i64{
 fn mul(a:i64,b:i64)->i64{
 	return a*b;
 }
+fn main(){
+println!("{}",toString(add(100,200)));
+println!("{}",toString(sub(200,100)));
+println!("{}",toString(mul(200,100)));
+println!("{}",toString(div(200,5)));
+}
